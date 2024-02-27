83d1de367109d035300f0db3c5925fd72e06e5f5
Ben Janecke <ben.janecke@operativa.dev>
2024-02-27 15:36:15 +0200

    feat(docs): adds CI/CD

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://verse.operativa.dev",
 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
@@ -16,8 +16,9 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "operativa",
+  organizationName: "operativa-dev",
   projectName: "verse",
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "throw",
