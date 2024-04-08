f13cf475213c478a7d97856dd4a0de439cc9b44b
William Francis <francisw1990@gmail.com>
2024-04-08 10:50:42 +0200

    fix(app): check if DB exists before rendering layout

--- apps/library/src/app/layout.tsx
+++ apps/library/src/app/layout.tsx
@@ -12,48 +12,52 @@ import { db } from "@/data";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Todo App",
+  title: "Library App",
 };
+
 const exists = await db.database.exists();
 if (!exists) {
   await db.database.recreate();
 }
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </head>
-      <body className={`${inter.className}  `}>
-        <TopMenu />
-        <CssVarsProvider disableTransitionOnChange>
-          <CssBaseline />
-          <Box sx={{ display: "flex", minHeight: "100dvh" }}>
-            <SideMenu />
-            <Box
-              component="main"
-              className="MainContent"
-              sx={{
-                px: { xs: 2, md: 6 },
-                pt: {
-                  xs: "calc(12px + var(--Header-height))",
-                  sm: "calc(12px + var(--Header-height))",
-                  md: 3,
-                },
-                pb: { xs: 2, sm: 2, md: 3 },
-                flex: 1,
-                display: "flex",
-                flexDirection: "column",
-                minWidth: 0,
-                height: "100dvh",
-                gap: 1,
-              }}
-            >
-              {children}
+  if (exists) {
+    return (
+      <html lang="en">
+        <head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </head>
+        <body className={`${inter.className}  `}>
+          <TopMenu />
+          <CssVarsProvider disableTransitionOnChange>
+            <CssBaseline />
+            <Box sx={{ display: "flex", minHeight: "100dvh" }}>
+              <SideMenu />
+              <Box
+                component="main"
+                className="MainContent"
+                sx={{
+                  px: { xs: 2, md: 6 },
+                  pt: {
+                    xs: "calc(12px + var(--Header-height))",
+                    sm: "calc(12px + var(--Header-height))",
+                    md: 3,
+                  },
+                  pb: { xs: 2, sm: 2, md: 3 },
+                  flex: 1,
+                  display: "flex",
+                  flexDirection: "column",
+                  minWidth: 0,
+                  height: "100dvh",
+                  gap: 1,
+                }}
+              >
+                {children}
+              </Box>
             </Box>
-          </Box>
-        </CssVarsProvider>
-      </body>
-    </html>
-  );
+          </CssVarsProvider>
+        </body>
+      </html>
+    );
+  }
+  return <>loading</>;
 }
